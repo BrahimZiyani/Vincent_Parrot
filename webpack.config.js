98855cc5aec4907a1eb78bb50f0627de2b8aba81
Brahim Ziyani <brahimziyani@live.fr>
2024-06-03 16:03:18 +0200

    Delete ux-turbo

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -68,6 +69,11 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    // Add the alias for controllers.json
+    .addAliases({
+        '@symfony/stimulus-bridge/controllers.json': path.resolve(__dirname, 'assets/controllers.json')
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
